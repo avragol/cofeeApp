c2da81b7d14a3f349ad6e214fa2b263d0f01b488
avrahamGol <avragol@gmail.com>
2024-07-09 15:55:07 +0300

    reset

--- public/script.js
+++ public/script.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function fetchCurrentTurn() {
-        fetch(`${window.env.SERVER_URL}/api/current-turn`)
+        fetch(`/api/current-turn`)
             .then(response => response.json())
             .then(data => updateUI(data))
             .catch(error => console.error('Error:', error));
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     nextTurnButton.addEventListener('click', () => {
         nextTurnButton.disabled = true;
-        fetch(`${window.env.SERVER_URL}/api/next-turn`, { method: 'POST' })
+        fetch(`/api/next-turn`, { method: 'POST' })
             .then(response => response.json())
             .then(data => {
                 updateUI(data);
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
     volunteerButtons.forEach(button => {
         button.addEventListener('click', () => {
             const name = button.dataset.volunteer;
-            fetch(`${window.env.SERVER_URL}/api/volunteer`, {
+            fetch(`/api/volunteer`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
